2042fe1310c862686f5aeef3869c151ddf2d16d0
Maxence Frenette <maxence.frenette@gmail.com>
2021-09-12 10:51:42 -0500

    Improve chess board styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChessBoard } from './components/ChessBoard';
 
 function App() {
+  const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
   return (
-    <ChessBoard />
+    <ChessBoard fen={fen} setFen={setFen} />
   );
 }
 
--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -4,14 +4,20 @@ import 'react-chessground/dist/styles/chessground.css';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    width: calc(100vh - 16px);
-    height: calc(100vh - 16px);
+    width: 100vh;
+    height: 100vh;
+    padding: 20px;
 `;
 
-export const ChessBoard = () => {
+export interface ChessBoardProps {
+    fen: string;
+    setFen: (newFen: string) => void;
+}
+
+export const ChessBoard: React.FC<ChessBoardProps> = ({ fen }) => {
     return (
         <Container>
-            <Chessground width="100%" height="100%" fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" />
+            <Chessground width="100%" height="100%" fen={fen} />
         </Container>
     );
 };
