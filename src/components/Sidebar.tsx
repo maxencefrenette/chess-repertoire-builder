280bc68f07c9b080894374b45919ad82395f6a87
Maxence Frenette <maxence.frenette@gmail.com>
2021-10-02 10:49:59 -0700

    Display lichess API data

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,35 @@
-import { Chess } from 'chess.ts'
-import React from 'react'
-import { useOpeningPosition } from '../hooks/api'
+import { Chess } from 'chess.ts';
+import React from 'react';
+import { OpeningMove, useOpeningPosition } from '../hooks/api';
 
 export interface SidebarProps {
     position: Chess;
 }
 
-
 export const Sidebar: React.FC<SidebarProps> = ({ position }) => {
-    const openingStats = useOpeningPosition(position.fen())
+    const openingStatsResponse = useOpeningPosition(position.fen());
+
+    if (!openingStatsResponse.data) {
+        return <div>Loading...</div>;
+    }
+
+    const openingStats = openingStatsResponse.data;
 
     return (
         <div>
-            {JSON.stringify(openingStats)}
+            <table>
+                {openingStats.moves.map((move) => (
+                    <tr>
+                        <td>{move.san}</td>
+                        <td>{score(move).toLocaleString(undefined, { style: 'percent' })}</td>
+                    </tr>
+                ))}
+            </table>
         </div>
-    )
+    );
+};
+
+function score(move: OpeningMove) {
+    const totalGames = move.white + move.draws + move.black;
+    return ((move.white + move.draws / 2)) / totalGames;
 }
