c017d2da03bbe6056e522c1abe91359b0256960c
Maxence Frenette <maxence.frenette@gmail.com>
2021-10-03 22:11:05 -0700

    Add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import CssBaseline from '@mui/material/CssBaseline';
 import { Layout } from './components/Layout';
 import { RootStore, StoreContext } from './store';
 
 function App() {
     return (
         <StoreContext.Provider value={new RootStore()}>
+            <CssBaseline />
             <Layout />
         </StoreContext.Provider>
     );
