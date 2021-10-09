b184b3306d9a2389ebe45f421b615f89b5b44963
Maxence Frenette <maxence.frenette@gmail.com>
2021-10-09 11:49:46 -0700

    Restrict board to legal moves

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import Chessground from '@react-chess/chessground';
 import { useStore } from '../store';
 import { observer } from 'mobx-react-lite';
+import { generateMovesForChessboard } from '../helpers/chess';
 
 export const ChessBoard: React.FC = observer(() => {
     const store = useStore();
+
+    const position = store.ui.position;
+    const moves = generateMovesForChessboard(position);
+
     return (
         <Chessground
             width={1000}
             height={1000}
-            config={{ fen: store.ui.position.fen() }}
+            config={{ fen: store.ui.position.fen(), movable: { free: false, dests: moves } }}
         />
     );
 });
---
+++ src/helpers/chess.ts
@@ -0,0 +1,31 @@
+import { Chess } from "chess.ts";
+
+export const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] as const;
+export type File = typeof files[number];
+export const ranks = ['1', '2', '3', '4', '5', '6', '7', '8'] as const;
+export type Rank = typeof ranks[number];
+
+function allChessSquares() {
+    const squares = new Array(64)
+    for (let i = 0; i < 64; i++) {
+        squares[i] = `${files[Math.floor(i / 8)]}${ranks[i % 8]}`;
+    }
+
+    return squares;
+}
+export const squares = allChessSquares();
+export type Square = `${File}${Rank}`
+
+export function generateMovesForChessboard(position: Chess): Map<Square, Square[]> {
+    const moves: Map<Square, Square[]> = new Map();
+
+    position.moves({ verbose: true}).forEach((m) => {
+        if (!moves.has(m.from as Square)) {
+            moves.set(m.from as Square, [m.to as Square])
+        } else {
+            moves.get(m.from as Square)?.push(m.to as Square)
+        }
+    })
+
+    return moves;
+}
