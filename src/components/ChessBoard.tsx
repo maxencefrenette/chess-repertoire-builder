9d2191078f3d8a2cbaeb9fc3a34bdc3a4e766106
Maxence Frenette <maxence.frenette@gmail.com>
2021-10-09 11:56:03 -0700

    MAllow making moves from chess board

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Chessground from '@react-chess/chessground';
 import { useStore } from '../store';
 import { observer } from 'mobx-react-lite';
-import { generateMovesForChessboard } from '../helpers/chess';
+import { generateMovesForChessboard, Square } from '../helpers/chess';
 
 export const ChessBoard: React.FC = observer(() => {
     const store = useStore();
@@ -14,7 +14,14 @@ export const ChessBoard: React.FC = observer(() => {
         <Chessground
             width={1000}
             height={1000}
-            config={{ fen: store.ui.position.fen(), movable: { free: false, dests: moves } }}
+            config={{
+                fen: store.ui.position.fen(),
+                movable: { free: false, dests: moves },
+                events: {
+                    move: (from, to) =>
+                        store.ui.makeMoveFromTo(from as Square, to as Square),
+                },
+            }}
         />
     );
 });
--- src/store/ui-state/index.ts
+++ src/store/ui-state/index.ts
@@ -1,17 +1,24 @@
 import { Chess } from "chess.ts";
-import { makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
+import { Square } from "../../helpers/chess";
 
 export class UiState {
     position: Chess;
 
     constructor() {
-        makeObservable(this, { position: observable })
+        makeObservable(this, { position: observable, makeMove: action, makeMoveFromTo: action })
         this.position = new Chess();
     }
 
     makeMove(move: string) {
         const newPosition = this.position.clone();
         newPosition.move(move);
+        this.position = newPosition;
+    }
+
+    makeMoveFromTo(from: Square, to: Square) {
+        const newPosition = this.position.clone();
+        newPosition.move({ from, to });
         this.position = newPosition;
     }
 }
