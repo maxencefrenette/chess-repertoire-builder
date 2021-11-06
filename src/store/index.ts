00f6cec2a986bb243ad4851c83f9ef8a4c9a1d6d
Maxence Frenette <maxence.frenette@gmail.com>
2021-11-06 13:33:41 -0700

    Remove repertoires from store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,10 @@
 import React from "react";
-import { Repertoire } from "./repertoire/Repertoire"
 import { UiState } from "./ui-state";
 
 export class RootStore {
-    repertoire: Repertoire;
     ui: UiState;
 
     constructor() {
-        this.repertoire = new Repertoire()
         this.ui = new UiState()
     }
 }
--- src/store/repertoire/Repertoire.ts
+++
@@ -1,9 +0,0 @@
-import { RepertoirePosition } from "./RepertoirePosition"
-
-export class Repertoire {
-    positions: Record<string, RepertoirePosition>
-
-    constructor() {
-        this.positions = {}
-    }
-}
