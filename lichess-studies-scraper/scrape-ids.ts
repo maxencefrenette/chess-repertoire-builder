5c74f705d78858e231e319cf826c01c1b77708e3
Maxence Frenette <maxence.frenette@gmail.com>
2021-11-14 22:14:34 -0800

    Scrape lichess studies ids

--- lichess-studies-scraper/scrape-ids.ts
+++ lichess-studies-scraper/scrape-ids.ts
@@ -1,15 +1,24 @@
 // Scrape lichess study IDs from the lichess website
 
 import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+import { createClient } from '@supabase/supabase-js';
 
-const url = 'https://lichess.org/study/all/popular';
+dotenv.config({ path: './.env.local' });
+const supabase = createClient(
+    'https://rvwnmvaaqxdydbgzvgfz.supabase.co',
+    process.env.SUPABASE_SERVICE_ROLE_KEY as string
+);
+
+// TODO scrape studies from other pages
+const lichessUrl = 'https://lichess.org/study/all/popular';
 
 (async () => {
     // Load page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'load', timeout: 0 });
-    console.log("Page Loaded");
+    await page.goto(lichessUrl, { waitUntil: 'load', timeout: 0 });
+    console.log('Page Loaded');
 
     // Scroll down to load all studies
     let lastHeight = await page.evaluate('document.body.scrollHeight');
@@ -22,25 +31,39 @@ const url = 'https://lichess.org/study/all/popular';
             break;
         }
         lastHeight = newHeight;
-        console.log("Loaded more studies");
+        console.log("Scrolled to load more studies");
     }
 
     // Get all study IDs
-    const studyLinkElements = await page.$$("a.overlay");
+    const studyLinkElements = await page.$$('a.overlay');
 
     if (studyLinkElements === null) {
-        throw new Error("StudyLinks query returned null");
+        throw new Error('StudyLinks query returned null');
     }
 
-    const studyIds = await Promise.all(studyLinkElements.map(async (link) => {
-        const hrefHandle = await link.getProperty("href");
-        const url: string = await hrefHandle.jsonValue();
-        
-        // TODO: assert that the url has the correct form
-        console.assert(url.length === 34);
+    const studyIds = await Promise.all(
+        studyLinkElements.map(async (link) => {
+            const hrefHandle = await link.getProperty('href');
+            const url: string = await hrefHandle.jsonValue();
 
-        return url.slice(26, 34);
-    }))
+            // TODO: assert that the url has the correct form
+            console.assert(url.length === 34);
 
+            return url.slice(26, 34);
+        })
+    );
+
+    console.log(`Found ${studyIds.length} studies`);
     console.log(studyIds);
-})();
+
+    const studyObjects = studyIds.map((id) => ({ id }));
+    const response = await supabase
+        .from('lichess_studies')
+        .upsert(studyObjects, { returning: 'minimal', ignoreDuplicates: true, count: 'exact' });
+
+    console.log(`Inserted ${response.count} new studies.`);
+    process.exit(0);
+})().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
