db890625046cb9bea9cfc40dc9dec9c69648920e
Maxence Frenette <maxence.frenette@gmail.com>
2021-11-20 22:22:02 -0600

    Add remove from repertoire button

--- frontend/src/api/supabase/api.ts
+++ frontend/src/api/supabase/api.ts
@@ -1,100 +1,124 @@
-import { Chess } from 'chess.ts';
-import { useQuery } from 'react-query';
-import { useSupabase, Position } from '.';
-import { games, LichessOpeningPosition } from '../lichess';
-import { Move } from './models/move';
+import { Chess } from "chess.ts";
+import { useQuery } from "react-query";
+import { useSupabase, Position } from ".";
+import { games, LichessOpeningPosition } from "../lichess";
+import { Move } from "./models/move";
 
 export function useRepertoirePosition(
-    repertoire_id: string | undefined,
-    fen: string
+  repertoire_id: string | undefined,
+  fen: string
 ) {
-    const supabase = useSupabase();
-    return useQuery(
-        ['positions', fen],
-        async () => {
-            const { data, error } = await supabase
-                .from<Position>('positions')
-                .select()
-                .eq('repertoire_id', repertoire_id!)
-                .eq('fen', fen)
-                .maybeSingle();
+  const supabase = useSupabase();
+  return useQuery(
+    ["positions", fen],
+    async () => {
+      const { data, error } = await supabase
+        .from<Position>("positions")
+        .select()
+        .eq("repertoire_id", repertoire_id!)
+        .eq("fen", fen)
+        .maybeSingle();
 
-            if (error !== null) {
-                throw error;
-            }
+      if (error !== null) {
+        throw error;
+      }
 
-            return data;
-        },
-        { enabled: !!repertoire_id }
-    );
+      return data;
+    },
+    { enabled: !!repertoire_id }
+  );
 }
 
 export function useRepertoirePositionMoves(
-    repertoire_id: string | undefined,
-    fen: string
+  repertoire_id: string | undefined,
+  fen: string
 ) {
-    const supabase = useSupabase();
-    return useQuery(
-        ['moves', fen],
-        async () => {
-            const { data, error } = await supabase
-                .from<Move>('moves')
-                .select()
-                .eq('repertoire_id', repertoire_id!)
-                .eq('parent_fen', fen);
+  const supabase = useSupabase();
+  return useQuery(
+    ["moves", fen],
+    async () => {
+      const { data, error } = await supabase
+        .from<Move>("moves")
+        .select()
+        .eq("repertoire_id", repertoire_id!)
+        .eq("parent_fen", fen);
 
-            if (error !== null) {
-                throw error;
-            }
+      if (error !== null) {
+        throw error;
+      }
 
-            return data!;
-        },
-        { enabled: !!repertoire_id }
-    );
+      return data!;
+    },
+    { enabled: !!repertoire_id }
+  );
 }
 
 export function useAddPositionToRepertoire() {
-    const supabase = useSupabase();
+  const supabase = useSupabase();
 
-    return async (
-        repertoirePosition: Position,
-        lichessOpeningPosition: LichessOpeningPosition,
-        moveSan: string
-    ) => {
-        const currentFen = repertoirePosition.fen;
-        const currentPosition = new Chess(repertoirePosition.fen);
-        const newPosition = currentPosition.clone();
-        newPosition.move(moveSan);
-        const newFen = newPosition.fen();
+  return async (
+    repertoirePosition: Position,
+    lichessOpeningPosition: LichessOpeningPosition,
+    moveSan: string
+  ) => {
+    const currentFen = repertoirePosition.fen;
+    const currentPosition = new Chess(repertoirePosition.fen);
+    const newPosition = currentPosition.clone();
+    newPosition.move(moveSan);
+    const newFen = newPosition.fen();
 
-        let moveFrequency = 1;
-        if (currentPosition.turn() === 'b') {
-            const totalGames = games(lichessOpeningPosition);
-            const gamesAfterMove = games(
-                lichessOpeningPosition.moves.find(
-                    (moveStats) => moveStats.san === moveSan
-                )!
-            );
+    let moveFrequency = 1;
+    if (currentPosition.turn() === "b") {
+      const totalGames = games(lichessOpeningPosition);
+      const gamesAfterMove = games(
+        lichessOpeningPosition.moves.find(
+          (moveStats) => moveStats.san === moveSan
+        )!
+      );
 
-            moveFrequency = gamesAfterMove / totalGames;
-        }
+      moveFrequency = gamesAfterMove / totalGames;
+    }
 
-        // TODO: account for transpositions
-        const childFrequency = repertoirePosition.frequency * moveFrequency;
+    // TODO: account for transpositions
+    const childFrequency = repertoirePosition.frequency * moveFrequency;
 
-        // TODO: move this to a stored procedure to make it faster and transactional
-        await supabase.from<Position>('positions').insert({
-            repertoire_id: repertoirePosition.repertoire_id,
-            fen: newFen,
-            frequency: childFrequency,
-        });
+    // TODO: move this to a stored procedure to make it faster and transactional
+    await supabase.from<Position>("positions").insert({
+      repertoire_id: repertoirePosition.repertoire_id,
+      fen: newFen,
+      frequency: childFrequency,
+    });
 
-        await supabase.from<Move>('moves').insert({
-            repertoire_id: repertoirePosition.repertoire_id,
-            parent_fen: currentFen,
-            child_fen: newFen,
-            move: moveSan,
-            move_frequency: moveFrequency,
-        });
-    };
+    await supabase.from<Move>("moves").insert({
+      repertoire_id: repertoirePosition.repertoire_id,
+      parent_fen: currentFen,
+      child_fen: newFen,
+      move: moveSan,
+      move_frequency: moveFrequency,
+    });
+  };
+}
+
+export function useRemovePositionFromRepertoire() {
+  const supabase = useSupabase();
+
+  return async (repertoire_id: string, fen: string) => {
+    // TODO: Recursively delete all unreachable positions
+    // TODO: Update frequency in child positions that are still reachable
+    // TODO: Move this to a stored procedure to make it faster and transactional
+    const p1 = supabase
+      .from<Position>("positions")
+      .delete()
+      .match({ repertoire_id, fen });
+    const p2 = supabase
+      .from<Position>("moves")
+      .delete()
+      .match({ repertoire_id, parent_fen: fen });
+    const p3 = supabase
+      .from<Position>("moves")
+      .delete()
+      .match({ repertoire_id, child_fen: fen });
+
+    await Promise.all([p1, p2, p3]);
+  };
 }
--- frontend/src/pages/chess-board-page/Sidebar.tsx
+++ frontend/src/pages/chess-board-page/Sidebar.tsx
@@ -1,192 +1,209 @@
-import { Box, Divider, IconButton, Paper, Tooltip } from '@mui/material';
-import { action } from 'mobx';
-import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { useLichessOpeningPosition, score, games } from '../../api/lichess';
-import { useStore } from '../../store';
-import { MovesBreadcrumbs } from './MovesBreadcrumbs';
-import { RepertoireSelect } from './RepertoireSelect';
-import AddIcon from '@mui/icons-material/Add';
+import { Box, Divider, IconButton, Paper, Tooltip } from "@mui/material";
+import { action } from "mobx";
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { useLichessOpeningPosition, score, games } from "../../api/lichess";
+import { useStore } from "../../store";
+import { MovesBreadcrumbs } from "./MovesBreadcrumbs";
+import { RepertoireSelect } from "./RepertoireSelect";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 import {
-    useRepertoirePosition,
-    useAddPositionToRepertoire,
-    useRepertoirePositionMoves,
-} from '../../api/supabase';
+  useRepertoirePosition,
+  useAddPositionToRepertoire,
+  useRepertoirePositionMoves,
+  useRemovePositionFromRepertoire,
+} from "../../api/supabase";
 import {
-    DataGrid,
-    GridColDef,
-    GridValueFormatterParams,
-} from '@mui/x-data-grid';
-import './Sidebar.css';
+  DataGrid,
+  GridColDef,
+  GridValueFormatterParams,
+} from "@mui/x-data-grid";
+import "./Sidebar.css";
 
 export const Sidebar: React.FC = observer(() => {
-    const store = useStore();
+  const store = useStore();
 
-    const fen = store.ui.position.fen();
-    const repertoireId = store.ui.repertoire?.id;
+  const fen = store.ui.position.fen();
+  const repertoireId = store.ui.repertoire?.id;
 
-    const { data: lichessOpeningStats } = useLichessOpeningPosition(fen);
-    const { data: repertoirePosition } = useRepertoirePosition(
-        repertoireId,
-        fen
-    );
-    const { data: repertoirePositionMoves } = useRepertoirePositionMoves(
-        repertoireId,
-        fen
-    );
-    const addPositionToRepertoire = useAddPositionToRepertoire();
+  const { data: lichessOpeningStats } = useLichessOpeningPosition(fen);
+  const { data: repertoirePosition } = useRepertoirePosition(repertoireId, fen);
+  const { data: repertoirePositionMoves } = useRepertoirePositionMoves(
+    repertoireId,
+    fen
+  );
+  const addPositionToRepertoire = useAddPositionToRepertoire();
+  const removePositionFromRepertoire = useRemovePositionFromRepertoire();
 
-    if (
-        !lichessOpeningStats ||
-        (repertoireId &&
-            (repertoirePosition === undefined ||
-                repertoirePositionMoves === undefined))
-    ) {
-        return <div>Loading...</div>;
-    }
+  if (
+    !lichessOpeningStats ||
+    (repertoireId &&
+      (repertoirePosition === undefined ||
+        repertoirePositionMoves === undefined))
+  ) {
+    return <div>Loading...</div>;
+  }
 
-    const handleAddToRepertoire = (moveSan: string) => {
-        addPositionToRepertoire(
-            repertoirePosition!,
-            lichessOpeningStats,
-            moveSan
-        );
-    };
+  const handleAddToRepertoire = (moveSan: string) => {
+    addPositionToRepertoire(repertoirePosition!, lichessOpeningStats, moveSan);
+  };
 
-    const repertoireSelected = store.ui.repertoire !== undefined;
-    const currentPositionIsInRepertoire = repertoirePosition !== null;
+  const handleRemoveFromRepertoire = (moveSan: string) => {
+    const position = store.ui.position.clone();
+    position.move(moveSan);
+    const childFen = position.fen();
 
-    const columns: GridColDef[] = [
-        {
-            field: 'add_to_repertoire',
-            headerName: '',
-            disableColumnMenu: true,
-            hideSortIcons: true,
-            width: 51,
-            align: 'center',
-            renderCell: ({ row }) => {
-                return (
-                    <Tooltip title="Add to repertoire">
-                        <IconButton
-                            color="primary"
-                            disabled={!currentPositionIsInRepertoire}
-                            onClick={() => handleAddToRepertoire(row.id)}
-                        >
-                            <AddIcon />
-                        </IconButton>
-                    </Tooltip>
-                );
+    removePositionFromRepertoire(store.ui.repertoire!.id, childFen);
+  };
+
+  const repertoireSelected = store.ui.repertoire !== undefined;
+  const currentPositionIsInRepertoire = repertoirePosition !== null;
+
+  const columns: GridColDef[] = [
+    {
+      field: "add_to_repertoire",
+      headerName: "",
+      disableColumnMenu: true,
+      hideSortIcons: true,
+      width: 51,
+      align: "center",
+      hide: !repertoireSelected,
+      renderCell: ({ row }) => {
+        if (!row.isInRepertoire) {
+          return (
+            <Tooltip title="Add to repertoire">
+              <IconButton
+                color="primary"
+                disabled={!currentPositionIsInRepertoire}
+                onClick={() => handleAddToRepertoire(row.id)}
+              >
+                <AddIcon />
+              </IconButton>
+            </Tooltip>
+          );
+        } else {
+          return (
+            <Tooltip title="Remove from repertoire">
+              <IconButton
+                color="error"
+                onClick={() => handleRemoveFromRepertoire(row.id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>
+          );
+        }
+      },
+    },
+    {
+      field: "id",
+      headerName: "Move",
+      width: 120,
+    },
+    {
+      field: "score",
+      headerName: "Score",
+      width: 120,
+      valueFormatter: formatPercent,
+    },
+    {
+      field: "games",
+      width: 120,
+      headerName: "Games",
+    },
+    {
+      field: "frequency",
+      headerName: "Frequency",
+      width: 120,
+      valueFormatter: formatFrequency,
+      hide: !repertoireSelected,
+    },
+  ];
+
+  // Zip repertoire moves with lichess moves data
+  const rows = store.ui.position
+    .moves()
+    .map((moveSan) => {
+      const lichessMove = lichessOpeningStats.moves.find(
+        (m) => m.san === moveSan
+      );
+
+      const repertoireMove = repertoirePositionMoves?.find(
+        (m) => m.move === moveSan
+      );
+
+      return {
+        id: moveSan,
+        score: lichessMove && score(lichessMove),
+        games: lichessMove && games(lichessMove),
+        frequency: repertoireMove && repertoireMove.move_frequency,
+        isInRepertoire: !!repertoireMove,
+      };
+    })
+    .filter((row) => row.games || row.frequency);
+
+  return (
+    <Paper>
+      <Box sx={{ padding: "16px" }}>
+        <RepertoireSelect />
+      </Box>
+      <Divider />
+      <Box sx={{ padding: "16px" }}>
+        <MovesBreadcrumbs />
+      </Box>
+      <Divider />
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        autoHeight={true}
+        hideFooter={true}
+        sortModel={[{ field: "games", sort: "desc" }]}
+        getRowClassName={({ row }) =>
+          row.isInRepertoire ? "row-in-repertoire" : ""
+        }
+        onRowClick={action(({ row }) => {
+          store.ui.hoveredMove = undefined;
+          store.ui.makeMove(row.id);
+        })}
+        componentsProps={{
+          row: {
+            onMouseEnter: (event: React.MouseEvent) => {
+              const moveSan = event.currentTarget.getAttribute("data-id")!;
+              store.ui.hoveredMove = moveSan;
             },
-        },
-        {
-            field: 'id',
-            headerName: 'Move',
-            width: 120,
-        },
-        {
-            field: 'score',
-            headerName: 'Score',
-            width: 120,
-            valueFormatter: formatPercent,
-        },
-        {
-            field: 'games',
-            width: 120,
-            headerName: 'Games',
-        },
-        {
-            field: 'frequency',
-            headerName: 'Frequency',
-            width: 120,
-            valueFormatter: formatFrequency,
-            hide: !repertoireSelected,
-        },
-    ];
+            onMouseLeave: (event: React.MouseEvent) => {
+              const moveSan = event.currentTarget.getAttribute("data-id")!;
 
-    const rows = store.ui.position
-        .moves()
-        .map((moveSan) => {
-            const lichessMove = lichessOpeningStats.moves.find(
-                (m) => m.san === moveSan
-            );
-
-            const repertoireMove = repertoirePositionMoves?.find(
-                (m) => m.move === moveSan
-            );
-
-            return {
-                id: moveSan,
-                score: lichessMove && score(lichessMove),
-                games: lichessMove && games(lichessMove),
-                frequency: repertoireMove && repertoireMove.move_frequency,
-                isInRepertoire: !!repertoireMove,
-            };
-        })
-        .filter((row) => row.games || row.frequency);
-
-    return (
-        <Paper>
-            <Box sx={{ padding: '16px' }}>
-                <RepertoireSelect />
-            </Box>
-            <Divider />
-            <Box sx={{ padding: '16px' }}>
-                <MovesBreadcrumbs />
-            </Box>
-            <Divider />
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                autoHeight={true}
-                hideFooter={true}
-                sortModel={[{ field: 'games', sort: 'desc' }]}
-                getRowClassName={({ row }) =>
-                    row.isInRepertoire ? 'row-in-repertoire' : ''
-                }
-                onRowClick={action(({ row }) => {
-                    store.ui.hoveredMove = undefined;
-                    store.ui.makeMove(row.id);
-                })}
-                componentsProps={{
-                    row: {
-                        onMouseEnter: (event: React.MouseEvent) => {
-                            const moveSan =
-                                event.currentTarget.getAttribute('data-id')!;
-                            store.ui.hoveredMove = moveSan;
-                        },
-                        onMouseLeave: (event: React.MouseEvent) => {
-                            const moveSan =
-                                event.currentTarget.getAttribute('data-id')!;
-
-                            if (store.ui.hoveredMove === moveSan) {
-                                store.ui.hoveredMove = undefined;
-                            }
-                        },
-                    },
-                }}
-            />
-        </Paper>
-    );
+              if (store.ui.hoveredMove === moveSan) {
+                store.ui.hoveredMove = undefined;
+              }
+            },
+          },
+        }}
+      />
+    </Paper>
+  );
 });
 
 function formatPercent(params: GridValueFormatterParams) {
-    return params.value?.toLocaleString(undefined, {
-        style: 'percent',
-        minimumFractionDigits: 2,
-    });
+  return params.value?.toLocaleString(undefined, {
+    style: "percent",
+    minimumFractionDigits: 2,
+  });
 }
 
 function formatFrequency(params: GridValueFormatterParams) {
-    const value = params.value;
+  const value = params.value;
 
-    if (typeof value !== 'number' || value <= 0) {
-        return;
-    } else if (value > 0.5) {
-        return value.toLocaleString(undefined, {
-            style: 'percent',
-            minimumFractionDigits: 2,
-        });
-    } else {
-        return `1 in ${Math.round(1 / value)}`;
-    }
+  if (typeof value !== "number" || value <= 0) {
+    return;
+  } else if (value > 0.5) {
+    return value.toLocaleString(undefined, {
+      style: "percent",
+      minimumFractionDigits: 2,
+    });
+  } else {
+    return `1 in ${Math.round(1 / value)}`;
+  }
 }
