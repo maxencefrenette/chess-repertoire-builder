592bb5dfaa298da347eb474c9eb380d71ecaa637
Maxence Frenette <maxence.frenette@gmail.com>
2021-11-26 20:33:50 -0800

    Tweak typings

--- database/seed.ts
+++ database/seed.ts
@@ -1,5 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
+// TODO: submit a bug report to supabase to they can fix their typings
+declare module "@supabase/supabase-js" {
+  interface UserAttributes {
+    email_confirm: boolean;
+  }
+}
+
 const supabase = createClient(
   process.env.SUPABASE_URL as string,
   process.env.SUPABASE_SERVICE_ROLE_KEY as string
@@ -9,4 +16,4 @@ await supabase.auth.api.createUser({
   email: "user@example.com",
   password: "QPP%&5b2CV&*Vxds",
   email_confirm: true,
-} as any);
+});
