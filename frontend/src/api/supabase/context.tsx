b3903ece18e69a7f89de5a57b2eb5ca36b50118c
Maxence Frenette <maxence.frenette@gmail.com>
2021-11-26 21:29:03 -0800

    Clean-up subscription

--- frontend/src/api/supabase/context.tsx
+++ frontend/src/api/supabase/context.tsx
@@ -1,7 +1,7 @@
-import React, { createContext, useContext, useEffect } from 'react';
-import { createClient } from '@supabase/supabase-js';
-import { useStore } from '../../store';
-import { action, runInAction } from 'mobx';
+import React, { createContext, useContext, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { useStore } from "../../store";
+import { action, runInAction } from "mobx";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
@@ -10,24 +10,28 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const SupabaseContext = createContext(supabase);
 
 export const SupabaseContextProvider: React.FC = ({ children }) => {
-    const store = useStore();
-    useEffect(() => {
-        runInAction(() => {
-            store.ui.isLoggedIn = supabase.auth.session() !== null;
-        });
-
-        supabase.auth.onAuthStateChange(
-            action((event, session) => {
-                store.ui.isLoggedIn = session !== null;
-            })
-        );
+  const store = useStore();
+  useEffect(() => {
+    runInAction(() => {
+      store.ui.isLoggedIn = supabase.auth.session() !== null;
     });
 
-    return (
-        <SupabaseContext.Provider value={supabase}>
-            {children}
-        </SupabaseContext.Provider>
+    const { data: sub } = supabase.auth.onAuthStateChange(
+      action((event, session) => {
+        store.ui.isLoggedIn = session !== null;
+      })
     );
+
+    return () => {
+      sub && sub.unsubscribe();
+    };
+  });
+
+  return (
+    <SupabaseContext.Provider value={supabase}>
+      {children}
+    </SupabaseContext.Provider>
+  );
 };
 
 export const useSupabase = () => useContext(SupabaseContext);
