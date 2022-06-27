abe183d13621140198acf66ab2cabd1302136b19
Maxence Frenette <maxence.frenette@gmail.com>
2022-06-27 01:46:50 -0700

    Add react-query devtools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import { Layout } from './components/Layout';
-import { RootStore, StoreContext } from './store';
-import './App.css';
-import { SupabaseContextProvider } from './api/supabase';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import CssBaseline from "@mui/material/CssBaseline";
+import { Layout } from "./components/Layout";
+import { RootStore, StoreContext } from "./store";
+import "./App.css";
+import { SupabaseContextProvider } from "./api/supabase";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
 function App() {
-    return (
-        <StoreContext.Provider value={new RootStore()}>
-            <SupabaseContextProvider>
-                <QueryClientProvider client={queryClient}>
-                    <CssBaseline />
-                    <Layout />
-                </QueryClientProvider>
-            </SupabaseContextProvider>
-        </StoreContext.Provider>
-    );
+  return (
+    <StoreContext.Provider value={new RootStore()}>
+      <SupabaseContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <CssBaseline />
+          <Layout />
+        </QueryClientProvider>
+      </SupabaseContextProvider>
+    </StoreContext.Provider>
+  );
 }
 
 export default App;
